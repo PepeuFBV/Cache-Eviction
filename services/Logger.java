40497ea7db5403c01b63d7553efbf58838c69a15
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-08-03 14:34:31 -0300

    Logger class implemented
    Logger class robustly implemented

--- services/Logger.java
+++ services/Logger.java
@@ -1,13 +1,58 @@
+package services;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Logger {
 
     private List<String> logs = new ArrayList<>();
-    private static String defaultLogPath = "../Log/log.txt";
+    private static final String defaultLogPath = "/Log/log.txt";
     private static Logger instance;
 
+    private Logger() {
+        try {
+            createFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    private void createFile() throws IOException {
+        try (FileWriter fileWriter = new FileWriter(Logger.defaultLogPath)) {
+            fileWriter.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void log(String message) {
-        logs.add(message);
+        logs.add("[" + java.time.LocalDateTime.now() + "] - " + message);
+    }
+
+    private void writeInFile(String path) throws IOException {
+        try (FileWriter fileWriter = new FileWriter(path)) {
+            for (String log : logs) {
+                fileWriter.write(log + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeLogsToDefaultPath() throws IOException {
+        writeInFile(defaultLogPath);
+    }
+
+    public void resetLogs() {
+        logs.clear();
     }
 }
