5b5aa62f62ce4182115ce4fbe7c811f76e506241
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-09-20 20:23:35 -0300

    HashTable sizing fixes

--- services/database/HashTable.java
+++ services/database/HashTable.java
@@ -2,52 +2,45 @@ package services.database;
 
 import entities.OS;
 import exceptions.NonExistentEntryException;
-
-import java.time.LocalDateTime;
 import java.util.LinkedList;
 
 public class HashTable {
 
-    private int m; // number for division method
-    private final int primeMultiplier; // prime number for hash function
+    private final int primeMultiplier; // prime number for extra randomness in the hash function
     private int capacity; // number of elements the hash table can store
-    private int size = 0; // number of elements in the hash table
+    private int size = 0; // number of elements currently in the hash table
     private boolean mayIncreaseCapacity = true;
+    private int threshold = 75 * 3; // percentage of the table that can be filled before increasing capacity (*3 for maximum of 225%)
     private LinkedList<OS>[] table;
 
+    @SuppressWarnings("unchecked")
     public HashTable() {
-        capacity = 127; // 0 - 126
-        m = 127;
-        primeMultiplier = 31;
-        table = new LinkedList[capacity];
+        this(31, 75);
+        this.capacity = 7; // initial capacity of 7 indexes
+        this.table = (LinkedList<OS>[]) new LinkedList[capacity];
     }
 
-    public HashTable(int capacity) {
-        this.capacity = new LargestPrimeNumber(capacity).getLargestPrime();
-        m = capacity;
-        primeMultiplier = 31;
-        table = new LinkedList[capacity];
+    public HashTable(int primeMultiplier) {
+        this(primeMultiplier, 75);
     }
 
-    public HashTable(int capacity, int primeMultiplier) {
-        this.capacity = new LargestPrimeNumber(capacity).getLargestPrime();
-        m = capacity;
-        if (primeMultiplier < 0) {
-            primeMultiplier = 31;
-        } else if (primeMultiplier > capacity) {
-            primeMultiplier = new LargestPrimeNumber(capacity).getLargestPrime(); // get the largest prime number smaller than capacity
+    @SuppressWarnings("unchecked")
+    public HashTable(int primeMultiplier, int threshold) {
+        this.capacity = 7; // initial capacity
+        this.table = (LinkedList<OS>[]) new LinkedList[capacity];
+        this.primeMultiplier = (primeMultiplier > 0) ? primeMultiplier : 31;
+        if (threshold > 0 && threshold <= 100) {
+            this.threshold = threshold;
         }
-        this.primeMultiplier = primeMultiplier;
-        table = new LinkedList[capacity];
     }
 
     public void setMayIncreaseCapacity(boolean mayIncreaseCapacity) {
         this.mayIncreaseCapacity = mayIncreaseCapacity;
     }
 
-    // hash function using division method
+    // hash function using division method with prime number multiplier to reduce collisions through extra randomness
     private int hash(int id) {
-        return (id * primeMultiplier) % m;
+        return (id * primeMultiplier) % capacity;
     }
 
     public boolean isFull() {
@@ -66,11 +59,9 @@ public class HashTable {
         return capacity;
     }
 
-    private void increaseCapacity() {
-        LinkedList<OS>[] oldTable = table;
-        capacity = new LargestPrimeNumber(capacity * 2).getLargestPrime();
-        this.m = capacity;
-        table = new LinkedList[capacity];
+    @SuppressWarnings("unchecked")
+    private LinkedList<OS>[] rehash(LinkedList<OS>[] oldTable) {
+        table = (LinkedList<OS>[]) new LinkedList[capacity];
         size = 0;
         for (LinkedList<OS> list : oldTable) { // rehashing old elements to the new table
             if (list != null) {
@@ -79,10 +70,17 @@ public class HashTable {
                 }
             }
         }
+        return table;
+    }
+
+    private void increaseCapacity() {
+        LinkedList<OS>[] oldTable = table;
+        capacity = new LargestPrimeNumber((int) Math.pow(capacity, 2)).getLargestPrime(); // get the largest prime number smaller than the square of the current capacity
+        table = rehash(oldTable);
     }
 
     public void add(OS serviceOrder) {
-        if (isFull() && mayIncreaseCapacity) {
+        if ((size >= (capacity * threshold / 100)) && mayIncreaseCapacity) {
             increaseCapacity();
         }
 
@@ -91,7 +89,7 @@ public class HashTable {
             table[index] = new LinkedList<>();
         }
 
-        for (OS os : table[index]) {
+        for (OS os : table[index]) { // check if service order already exists index List
             if (os.getId() == serviceOrder.getId()) {
                 return;
             }
@@ -113,6 +111,10 @@ public class HashTable {
         return null;
     }
 
+    public void remove(OS os) throws NonExistentEntryException {
+        remove(os.getId());
+    }
+
     public void remove(int key) throws NonExistentEntryException {
         int index = hash(key);
         if (table[index] != null) {
@@ -126,9 +128,8 @@ public class HashTable {
                     return;
                 }
             }
-        } else {
-            throw new NonExistentEntryException("Service Order ID " + key + " not found in database");
         }
+        throw new NonExistentEntryException("Service Order ID " + key + " not found in database");
     }
 
     @Override
@@ -140,7 +141,7 @@ public class HashTable {
                 sb.append(listI).append(": [ ");
                 for (OS os : list) {
                     if (os != null) {
-                        sb.append(os.toString()).append(", ");
+                        sb.append(os).append(", ");
                     }
                 }
                 sb.append(" ]\n");
@@ -150,4 +151,5 @@ public class HashTable {
         }
         return sb.toString();
     }
+
 }
