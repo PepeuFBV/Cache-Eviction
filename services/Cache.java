93bdef2963e1f941a8b42bab6e373aa129b23bc1
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-09-20 20:23:23 -0300

    Cache fixes

--- services/Cache.java
+++ services/Cache.java
@@ -1,35 +1,41 @@
 package services;
 
 import entities.OS;
-import services.database.LargestPrimeNumber;
 
-import java.time.LocalDateTime;
-
-// uses hash logic to store up to 20 service orders
+// - 1 because the array is 0-indexed
 public class Cache {
 
-    // stores the last 20 service orders
-    public final int maxSize = 20;
-    private int m;
-    private int primeMultiplier;
-    private int capacity;
+    public final int capacity = 20;
     private int size = 0;
-    private OS[] table;
+    private final OSK[] table;
+    private final int kIterationLimit = 10;
 
-    public Cache() {
-        capacity = 20;
-        m = 20;
-        primeMultiplier = new LargestPrimeNumber(20).getLargestPrime();
-        table = new OS[capacity];
+    private class OSK {
+        public final OS os;
+        public int k;
+
+        public OSK(OS os, int k) {
+            this.os = os;
+            this.k = k;
+        }
+
     }
 
-    // hash function using division method
+    public Cache() {
+        table = new OSK[capacity];
+    }
+
     private int hash(int id) {
-        return (id * primeMultiplier) % m;
+        return this.hash(id, 0);
+    }
+
+    // h(x, k) = (x * 31 + k * 17) % 20
+    private int hash(int id, int k) {
+        return (id * 31 + k * 17) % capacity;
     }
 
     public boolean isFull() {
-        return size == maxSize;
+        return size == capacity;
     }
 
     public boolean isEmpty() {
@@ -40,40 +46,76 @@ public class Cache {
         return size;
     }
 
-    // doesn't care if the cache is full, will overwrite the service order in the index
+
     public void add(OS os) {
-        int index = hash(os.getId());
-        if (table[index] != null) {
-            size--;
+        if (!isFull()) {
+            for (int k = 0; k < capacity; k++) {
+                int index = hash(os.getId(), k);
+                if (table[index] == null) {
+                    table[index] = new OSK(os, k);
+                    size++;
+                    return;
+                }
+            }
+        } else { // remove the OS in the new index placement
+            int index = hash(os.getId());
+            if (table[index] != null) {
+                table[index] = null;
+                table[index] = new OSK(os, 0);
+            }
         }
-        table[index] = os;
-        size++;
     }
 
     public void remove(int key) {
-        int index = hash(key);
-        if (table[index] != null) {
-            table[index] = null;
-            size--;
+        for (int k = 0; k < capacity; k++) {
+            int index = hash(key, k);
+            if (table[index] != null && table[index].os.getId() == key) {
+                table[index] = null;
+                size--;
+                return;
+            }
+        }
+    }
+
+    public void remove(OS os) {
+        for (int k = 0; k < capacity; k++) {
+            int index = hash(os.getId(), k);
+            if (table[index] != null && table[index].os.equals(os)) {
+                table[index] = null;
+                size--;
+                return;
+            }
         }
     }
 
     public boolean isInCache(OS os) {
-        int index = hash(os.getId());
-        return table[index] != null && table[index].equals(os);
+        for (int k = 0; k < capacity; k++) { // iterate through all possible k values
+            int index = hash(os.getId(), k);
+            if (table[index] != null && table[index].os.equals(os)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public OS search(int key) {
-        int index = hash(key);
-        return table[index];
+        for (int k = 0; k < capacity; k++) {
+            int index = hash(key, k);
+            if (table[index] != null && table[index].os.getId() == key) {
+                return table[index].os;
+            }
+        }
+        return null;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (OS os : table) {
-            if (os != null) {
-                sb.append(os.toString()).append("\n");
+        for (OSK osk : table) {
+            if (osk != null) {
+                if (osk.os != null) {
+                    sb.append(osk.os.toString()).append("\n");
+                }
             }
         }
         return sb.toString();
