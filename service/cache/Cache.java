4a5fcbaf61603d216533e2ac1ca0882272064c9e
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-10-13 23:05:56 -0300

    Cache class fixes

--- service/cache/Cache.java
+++ service/cache/Cache.java
@@ -7,15 +7,11 @@ import service.Logger;
 // capacity: 30
 public class Cache {
 
-    private static String getCurrentTime() {
-        return java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("HH:mm:ss"));
-    }
-
     private final Logger logger;
     private final PriorityQueue cache;
 
-    public Cache(Logger logger) {
-        this.logger = logger;
+    public Cache() {
+        this.logger = new Logger(Logger.LogOrigin.CACHE);
         this.cache = new PriorityQueue();
     }
 
@@ -28,58 +24,69 @@ public class Cache {
     }
 
     public void add(OS os) {
+        logger.log("Adding a new element to cache with ID " + os.getId());
         int capacity = 30;
         if (cache.size() == capacity) {
+            logger.log("Cache is full");
             cache.remove(); // remove the last element from cache (lowest priority)
-            logger.log("[" + getCurrentTime() + "] Removed the first element from cache");
+            logger.log("Removed the first element from cache");
         }
         cache.insert(new CacheEntry(os));
-        logger.log("[" + getCurrentTime() + "] Added a new element to cache");
+        logger.log("Added the new element to cache");
     }
 
     public boolean isInCache(OS os) {
+        logger.log("Checking if element is in cache");
         for (CacheEntry cacheEntry : cache) {
             if (cacheEntry.getOs() == os) {
+                logger.log("Element found in cache");
                 return true;
             }
         }
+        logger.log("Element not found in cache");
         return false;
     }
 
     public boolean isInCache(int id) {
+        logger.log("Checking if element is in cache with ID " + id);
         for (CacheEntry cacheEntry : cache) {
             if (cacheEntry.getOs().getId() == id) {
+                logger.log("Element found in cache");
                 return true;
             }
         }
+        logger.log("Element not found in cache");
         return false;
     }
 
     public OS search(int id) {
+        logger.log("Searching for element in cache with ID " + id);
         for (CacheEntry cacheEntry : cache) {
             if (cacheEntry.getOs().getId() == id) {
-                logger.log("[" + getCurrentTime() + "] Searched for element in cache");
+                logger.log("Found element in cache");
                 return cache.get(id).getOs();
             }
         }
-        logger.log("[" + getCurrentTime() + "] Element not found in cache");
+        logger.log("Element not found in cache");
         return null;
     }
 
     // method only for using when explicitly removing an element from the cache
     public void remove(int id) throws NonExistentEntryException {
+        logger.log("Removing element from cache with ID " + id);
         try {
             cache.remove(id);
-            logger.log("[" + getCurrentTime() + "] Removed element from cache");
+            logger.log("Removed element from cache");
         } catch (NonExistentEntryException e) {
-            logger.log("[" + getCurrentTime() + "] " + e.getMessage());
+            logger.log("Element not found in cache");
             throw new NonExistentEntryException(e.getMessage());
         }
     }
 
     public void clearCache() {
+        logger.log("Clearing cache");
         cache.clear();
-        logger.log("[" + getCurrentTime() + "] Cache cleared");
+        logger.log("Cache cleared");
     }
 
     public boolean isEmpty() {
