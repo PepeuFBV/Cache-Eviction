35461a497da1f6b652b06b2f4b29f73e342d7094
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-10-14 14:35:41 -0300

    Created AutoAdjustableList class + HashTable usage

---
+++ service/database/AutoAdjustableList.java
@@ -0,0 +1,86 @@
+package service.database;
+
+import java.util.Arrays;
+
+public class AutoAdjustableList<T> implements Iterable<T> {
+
+    private static final int INITIAL_CAPACITY = 3;
+    private Object[] elements;
+    private int size = 0;
+
+    public AutoAdjustableList() {
+        elements = new Object[INITIAL_CAPACITY];
+    }
+
+    public void add(T element) {
+        if (isFull()) {
+            increaseCapacity();
+        }
+        elements[size++] = element;
+    }
+
+    public boolean remove(T element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                elements[i] = elements[--size];
+                elements[size] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return (T) elements[index];
+    }
+
+    private void increaseCapacity() {
+        int newCapacity = elements.length * 2;
+        elements = Arrays.copyOf(elements, newCapacity);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isFull() {
+        return size == elements.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                return get(index++);
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]).append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+}
--- service/database/HashTable.java
+++ service/database/HashTable.java
@@ -4,8 +4,6 @@ import entities.OS;
 import exceptions.NonExistentEntryException;
 import service.log.Logger;
 
-import java.util.LinkedList;
-
 public class HashTable {
 
     private final int primeMultiplier; // prime number for extra randomness in the hash function
@@ -13,14 +11,14 @@ public class HashTable {
     private int size = 0; // number of elements currently in the hash table
     private boolean mayIncreaseCapacity = true;
     private int threshold; // percentage of the table that can be filled before increasing capacity (default is 250%)
-    private LinkedList<OS>[] table;
+    private AutoAdjustableList<OS>[] table;
     private final Logger logger;
 
     @SuppressWarnings("unchecked")
     public HashTable() {
         this(31, 225);
-        this.capacity = 7; // initial capacity of 7 indexes
-        this.table = (LinkedList<OS>[]) new LinkedList[capacity];
+        this.capacity = 7;
+        this.table = (AutoAdjustableList<OS>[]) new AutoAdjustableList[capacity];
     }
 
     public HashTable(int primeMultiplier) {
@@ -29,8 +27,8 @@ public class HashTable {
 
     @SuppressWarnings("unchecked")
     public HashTable(int primeMultiplier, int threshold) {
-        this.capacity = 7; // initial capacity
-        this.table = (LinkedList<OS>[]) new LinkedList[capacity];
+        this.capacity = 7;
+        this.table = (AutoAdjustableList<OS>[]) new AutoAdjustableList[capacity];
         this.primeMultiplier = (primeMultiplier > 0) ? primeMultiplier : 31;
         if (threshold > 0 ) {
             this.threshold = threshold;
@@ -65,14 +63,14 @@ public class HashTable {
     }
 
     @SuppressWarnings("unchecked")
-    private LinkedList<OS>[] rehash(LinkedList<OS>[] oldTable) {
+    private AutoAdjustableList<OS>[] rehash(AutoAdjustableList<OS>[] oldTable) {
         logger.log("Rehashing Service Orders to new table");
-        table = (LinkedList<OS>[]) new LinkedList[capacity];
+        table = (AutoAdjustableList<OS>[]) new AutoAdjustableList[capacity];
         size = 0;
-        for (LinkedList<OS> list : oldTable) { // rehashing old elements to the new table
+        for (AutoAdjustableList<OS> list : oldTable) {
             if (list != null) {
                 for (OS os : list) {
-                    add(os); // runs with the new m value
+                    add(os);
                 }
             }
         }
@@ -81,8 +79,8 @@ public class HashTable {
     }
 
     private void increaseCapacity() {
-        LinkedList<OS>[] oldTable = table;
-        capacity = new LargestPrimeNumber(capacity * 2).getLargestPrime(); // get the largest prime number smaller than double of the current capacity
+        AutoAdjustableList<OS>[] oldTable = table;
+        capacity = new LargestPrimeNumber(capacity * 2).getLargestPrime();
         logger.log("Increasing capacity to " + capacity);
         table = rehash(oldTable);
     }
@@ -94,13 +92,12 @@ public class HashTable {
         }
 
         int index = hash(serviceOrder.getId());
-        if (table[index] == null) { // creates a new LinkedList if the index is empty
-            logger.log("Creating new LinkedList at index " + index);
-            table[index] = new LinkedList<>();
-        }
-        else { // adds to the end of the LinkedList if the index is not empty
-            for (OS os : table[index]) { // checks if the OS is already in the database
-                if (os.getId() == serviceOrder.getId()) { // id is unique
+        if (table[index] == null) {
+            logger.log("Creating new AutoAdjustableList at index " + index);
+            table[index] = new AutoAdjustableList<>();
+        } else {
+            for (OS os : table[index]) {
+                if (os.getId() == serviceOrder.getId()) {
                     logger.log("Service Order ID " + serviceOrder.getId() + " already exists in the database");
                     return;
                 }
@@ -109,7 +106,6 @@ public class HashTable {
         logger.log("Adding Service Order to index " + index);
         table[index].add(serviceOrder);
         size++;
-
     }
 
     public OS search(int key) {
@@ -141,7 +137,7 @@ public class HashTable {
                     table[index].remove(os);
                     size--;
                     if (table[index].isEmpty()) {
-                        logger.log("LinkedList at index " + index + " is empty, removing it");
+                        logger.log("AutoAdjustableList at index " + index + " is empty, removing it");
                         table[index] = null;
                     }
                     logger.log("Service Order ID " + key + " removed from database");
@@ -173,7 +169,7 @@ public class HashTable {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         int listI = 0;
-        for (LinkedList<OS> list : table) {
+        for (AutoAdjustableList<OS> list : table) {
             if (list != null) {
                 sb.append(listI).append(": [ ");
                 for (OS os : list) {
